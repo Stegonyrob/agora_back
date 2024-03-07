73fe0396ff83f455f60fb383495fe3f07ac89fda
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-03-07 13:27:15 +0100

    first commit

---
+++ src/main/java/de/stella/agora_web/AgoraWebApplication.java
@@ -0,0 +1,13 @@
+package de.stella.agora_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgoraWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgoraWebApplication.class, args);
+	}
+
+}
