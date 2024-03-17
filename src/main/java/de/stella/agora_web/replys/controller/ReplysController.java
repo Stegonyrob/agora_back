c1edb59454ffdc5a720476fe20fe5b592d2d7faa
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-03-17 12:13:09 +0100

    add endpoint replies/id test whit swagger

--- src/main/java/de/stella/agora_web/replys/controller/ReplysController.java
+++ src/main/java/de/stella/agora_web/replys/controller/ReplysController.java
@@ -3,6 +3,8 @@ package de.stella.agora_web.replys.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import de.stella.agora_web.replys.controller.dto.ReplyDTO;
 import de.stella.agora_web.replys.model.Reply;
 import de.stella.agora_web.replys.services.IReplyService;
+import io.micrometer.common.lang.NonNull;
 
 @RestController
 @RequestMapping(path = "${api-endpoint}/replies")
@@ -28,4 +31,11 @@ public class ReplysController {
         Reply reply = replyService.createReply(replyDTO, null);
         return ResponseEntity.status(HttpStatus.CREATED).body(reply);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reply> show(@NonNull @PathVariable("id") Long id) {
+        Reply reply = replyService.getReplyById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(reply);
+    }
+
 }
