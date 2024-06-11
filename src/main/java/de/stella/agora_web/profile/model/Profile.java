238da413242f31bf69e95b6214c700e5d296a39c
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-06-11 21:42:02 +0200

    insert profile model

---
+++ src/main/java/de/stella/agora_web/profile/model/Profile.java
@@ -0,0 +1,36 @@
+// package de.stella.agora_web.profile.model;
+
+// import de.stella.agora_web.user.model.User;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.Setter;
+
+
+//     @NoArgsConstructor
+// @Getter
+// @Setter
+// @Entity(name = "Profile")
+// @Table(name = "profiles")
+// public class Profile {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+
+//     private User user;
+
+//     private String email;
+//     private String firstName;
+//     private String firstLastName;
+//     private String secondLastName;
+//     private String address;
+//     private String city;
+//     private String province;
+//     private String postalCode;
+//     private String numberPhone;
+// }
